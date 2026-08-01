ed104c6b4a4792770c558f513c411d5d890000c5
Vladislav <cankerousrook7@gmail.com>
2026-08-01 16:56:24 +0600

    Application added

---
+++ src/test/java/kg/attractor/payment_system/PaymentSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package kg.attractor.payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
